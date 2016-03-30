e26336b05d99d095e95ffc3128b2ed66389fa2c7
Yash Kulshrestha <YashdalfTheGray@users.noreply.github.com>
2016-03-29 21:03:31 -0400

    updated for temperature and humidity

--- particle/sensor-hub.cpp
+++ particle/sensor-hub.cpp
@@ -1,10 +1,17 @@
-char *versionString = "1.1.0";
+// This #include statement was automatically added by the Particle IDE.
+#include "Adafruit_DHT/Adafruit_DHT.h"
+
+#define DHTPIN 5
+#define DHTTYPE DHT11
+
+char *versionString = "1.2.0";
 
 int photoResistorValue = 0;
 int potValue = 0;
 int tempSensorValue = 0;
+int humiditySensorValue = 0;
 
-double temperature = 0.0;
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
     if (Particle.variable("lightSensor", photoResistorValue)) {
@@ -13,8 +20,11 @@ void setup() {
     if (Particle.variable("trimPot", potValue)) {
         Particle.publish("Variable trimPot published", "", 60, PRIVATE);
     }
-    if (Particle.variable("temperature", temperature)) {
+    if (Particle.variable("temperature", tempSensorValue)) {
         Particle.publish("Variable temperature published", "", 60, PRIVATE);
+    }
+    if (Particle.variable("humidity", humiditySensorValue)) {
+        Particle.publish("Variable humidity published", "", 60, PRIVATE);
     }
     if (Particle.variable("version", versionString)) {
         Particle.publish("Variable version published", "", 60, PRIVATE);
@@ -22,14 +32,17 @@ void setup() {
     
     pinMode(A0, INPUT);
     pinMode(A1, INPUT);
-    pinMode(D5, INPUT);
+    
+    dht.begin();
 }
 
 void loop() {
     photoResistorValue = analogRead(A0);
     potValue = analogRead(A1);
     
-    temperature = (((tempSensorValue * 3.3)/4095) - 0.5) * 100;
+    tempSensorValue = dht.getTempFarenheit();
     
-    delay(500);
+    humiditySensorValue = dht.getHumidity();
+    
+    delay(1000);
 }
