7678f062d2523d4a03a35d5a24ec091041f64104
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-04 22:37:16 -0400

    started to mess around with http requests, can not make them for whatever reason

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,7 +1,21 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { Response } from 'angular2/http';
+
+import { ParticleService } from './particle.service';
 
 @Component({
     selector: 'ieee-app',
-    templateUrl: 'app/app.tpl.html'
+    templateUrl: 'app/app.tpl.html',
+    providers: [
+        ParticleService
+    ]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    constructor(public particleService: ParticleService) {}
+
+    ngOnInit() {
+        this.particleService.getDevices().subscribe((res: Response) => {
+            console.log(res.json());
+        });
+    }
+}
--- client/app/particle.service.ts
+++ client/app/particle.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from 'angular2/core';
-import { Http, Response } from 'angular2/http';
+import { Http, Response, RequestOptions, Headers } from 'angular2/http';
+import { Observable } from '../node_modules/rxjs/Rx.d.ts';
 
 @Injectable()
 export class ParticleService {
     constructor(public http: Http) { }
 
-    getDevices(authToken) {
-
+    getDevices(): Observable<Response> {
+        console.log('getDevices() called!')
+        var reqOptions = new RequestOptions({
+            headers: new Headers({
+                'Authorization': 'Bearer ieee-sac-2016-workshop'
+            })
+        });
+        return this.http.get('https://particle-proxy.herokuapp.com/api/v1/devices', reqOptions);
     }
 
-    getVariable(authToken, deviceId, variable) {
-
+    getVariable(deviceId: string, variable: string): Observable<Response> {
+        var reqOptions = new RequestOptions({
+            headers: new Headers({
+                'Authorization': 'Bearer ieee-sac-2016-workshop'
+            })
+        });
+        return this.http.get('https://particle-proxy.herokuapp.com/api/v1/devices', reqOptions);
     }
 }
