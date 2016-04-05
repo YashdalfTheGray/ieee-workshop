64b2ba9a9e5a12be78c90543bd230ad5fcbb905e
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-04 21:56:35 -0400

    added http providers to the main bootstrap

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,4 +1,7 @@
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
+import { bootstrap }    from 'angular2/platform/browser';
+import { HTTP_PROVIDERS } from 'angular2/http';
+import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+    HTTP_PROVIDERS
+]);
