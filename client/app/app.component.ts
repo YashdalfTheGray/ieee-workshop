2843eb52809d53d9edd3b37abc482d8668b84601
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-06 21:24:09 -0400

    added code to get all the variables from all the devices

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from 'angular2/core';
 import { Response } from 'angular2/http';
 
 import { ParticleService } from './particle.service';
+import { Device } from './device.types';
 
 @Component({
     selector: 'ieee-app',
@@ -11,12 +12,21 @@ import { ParticleService } from './particle.service';
     ]
 })
 export class AppComponent implements OnInit {
-    private res: Array<Object>;
+    private res: Array<Device>;
     constructor(public particleService: ParticleService) {}
 
     ngOnInit() {
         this.particleService.getDevices().subscribe((res: Response) => {
             this.res = res.json();
+            this.res.forEach(device => {
+                device.variables = {};
+                ['temperature', 'lightSensor', 'humidity'].forEach((sensor: string) => {
+                    this.particleService.getVariable(device.id, sensor).subscribe((res: Response) => {
+                        var sensorResponse = res.json();
+                        device.variables[sensorResponse.name] = sensorResponse.result;
+                    });
+                });
+            });
         });
     }
 }
